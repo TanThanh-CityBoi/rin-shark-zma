f7294abc409196adcbe6b955a583ae51e953ad90
TanThanh-CityBoi <tanthanhe@gmail.com>
2024-05-04 15:28:02 +0700

    feat: follow OA

--- src/components/product/picker.tsx
+++ src/components/product/picker.tsx
@@ -28,7 +28,7 @@ function getDefaultOptions(product?: Product) {
         Object.assign(options, {
           [variant.id]: variant.default,
         }),
-      {},
+      {}
     );
   }
   return {};
@@ -41,7 +41,7 @@ export const ProductPicker: FC<ProductPickerProps> = ({
 }) => {
   const [visible, setVisible] = useState(false);
   const [options, setOptions] = useState<SelectedOptions>(
-    selected ? selected.options : getDefaultOptions(product),
+    selected ? selected.options : getDefaultOptions(product)
   );
   const [quantity, setQuantity] = useState(1);
   const setCart = useSetRecoilState(cartState);
@@ -62,7 +62,7 @@ export const ProductPicker: FC<ProductPickerProps> = ({
           const editing = cart.find(
             (item) =>
               item.product.id === product.id &&
-              isIdentical(item.options, selected.options),
+              isIdentical(item.options, selected.options)
           )!;
           if (quantity === 0) {
             res.splice(cart.indexOf(editing), 1);
@@ -71,7 +71,7 @@ export const ProductPicker: FC<ProductPickerProps> = ({
               (item, i) =>
                 i !== cart.indexOf(editing) &&
                 item.product.id === product.id &&
-                isIdentical(item.options, options),
+                isIdentical(item.options, options)
             )!;
             res.splice(cart.indexOf(editing), 1, {
               ...editing,
@@ -87,7 +87,7 @@ export const ProductPicker: FC<ProductPickerProps> = ({
           const existed = cart.find(
             (item) =>
               item.product.id === product.id &&
-              isIdentical(item.options, options),
+              isIdentical(item.options, options)
           );
           if (existed) {
             res.splice(cart.indexOf(existed), 1, {
@@ -158,7 +158,7 @@ export const ProductPicker: FC<ProductPickerProps> = ({
                           }))
                         }
                       />
-                    ),
+                    )
                   )}
                 <QuantityPicker value={quantity} onChange={setQuantity} />
                 {selected ? (
@@ -189,7 +189,7 @@ export const ProductPicker: FC<ProductPickerProps> = ({
             </Box>
           )}
         </Sheet>,
-        document.body,
+        document.body
       )}
     </>
   );
--- src/pages/cart/preview.tsx
+++ src/pages/cart/preview.tsx
@@ -1,13 +1,32 @@
 import { DisplayPrice } from "components/display/price";
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 import { totalPriceState, totalQuantityState } from "state";
-import pay from "utils/product";
 import { Box, Button, Text } from "zmp-ui";
+import { followOA, getUserInfo } from "zmp-sdk/apis";
+import pay from "utils/product";
 
 export const CartPreview: FC = () => {
   const quantity = useRecoilValue(totalQuantityState);
   const totalPrice = useRecoilValue(totalPriceState);
+  const [visible, setVisible] = useState(false);
+
+  const [followedOA, setFollowedOA] = useState(false);
+
+  useEffect(() => {
+    getUserInfo({
+      success: (data) => {
+        // xử lý khi gọi api thành công
+        const { userInfo } = data;
+        console.log("user infooo: ", userInfo);
+        setFollowedOA(userInfo.followedOA || false);
+      },
+      fail: (error) => {
+        // xử lý khi gọi api thất bại
+        console.log(error);
+      },
+    });
+  }, []);
 
   return (
     <Box flex className="sticky bottom-0 bg-background p-4 space-x-4">
@@ -28,7 +47,20 @@ export const CartPreview: FC = () => {
         type="highlight"
         disabled={!quantity}
         fullWidth
-        onClick={() => pay(totalPrice)}
+        onClick={() => {
+          if (!followedOA)
+            followOA({
+              id: "386237860505888586",
+              success: () => {
+                setFollowedOA(true);
+                console.log("follow success");
+              },
+              fail: (err) => {},
+            });
+          else {
+            pay(quantity, "Đặt hàng handler");
+          }
+        }}
       >
         Đặt hàng
       </Button>
